66948ab3bd89c64a65057bdb196ba61744f91f5c
Aquib <aquibshaikh02397@gmail.com>
2020-12-03 14:51:43 +0530

    added toggle

--- src/components/App.js
+++ src/components/App.js
@@ -155,35 +155,76 @@ const states = [
 ];
 
 function App() {
-  const [city, setCity] = useState(0);
-  const [state, setState] = useState(0);
-  const town =states[state].cities[city].towns.map((town,tid)=>(
-    <div id={"town"+(tid+1)} key={town.name+tid}>{town}</div>
-  ));
-  console.log(town);
+  const [city, setCity] = useState(-1);
+  const [state, setState] = useState(-1);
+  // const town = states[state].cities[city].towns.map((town, tid) => (
+  //   <div id={"town" + (tid + 1)} key={town.name + tid}>
+  //     {town}
+  //   </div>
+  // ));
+  // console.log(town);
+  // const getCities = (id) => {
+  //   console.log(id, state);
+  //   if (id == state) {
+  //     const b = states[state].cities.map((c, cid) => {
+  //       <div
+  //         className="city"
+  //         key={c.name + cid}
+  //         id={"city" + (cid + 1)}
+  //         onClick={() => (city === cid ? setCity(-1) : setCity(id))}
+  //       >
+  //         {c.name}
+  //       </div>;
+  //     });
+  //     return b;
+  //   }
+  //   return null;
+  // };
+  // const getTown = (id) => {
+  //   {
+  //     return city == id
+  //       ? states[state].cities[city].towns.map((town, tid) => (
+  //           <div className="town" id={"town" + (tid + 1)} key={town.name}>
+  //             {town.name}
+  //           </div>
+  //         ))
+  //       : null;
+  //   }
+  // };
+  const getMethods = (obj) => {
+    let properties = new Set()
+    let currentObj = obj
+    do {
+      Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+    } while ((currentObj = Object.getPrototypeOf(currentObj)))
+    return [...properties.keys()].filter(item => typeof obj[item] === 'function')
+  }
   return (
     <div id="main">
-      
-        {states.map((st, id) => (
-          <div
-            className="state"
-            id={"state" + (id + 1)}
-            key={st.name + id}
-            onClick={() => setState(id)}
-          >
-            <h1>{st.name}</h1>
-            { id==state?
+      {states.map((st, id) => (
+        <div
+          className="state"
+          id={"state" + (id + 1)}
+          key={st.name + id}
+          onClick={(event) => {if(event.target.tagName=="H1"){
+            id===state ?setState(-1) :setState(id);
+            setCity(-1);
+          } }}
+        >
+          <h1>{st.name}</h1>
+          {/* {getCities(id)} */}
+          { state >= 0 && id==state ?
           
                 states[state].cities.map((ci, cityId) => (
                   <div
                     className="city"
                     id={"city" + (cityId + 1)}
                     key={ci.name + cityId}
-                    onClick={() => setCity(cityId)}
+                    onClick={(event) => {city===cityId? setCity(-1) :setCity(cityId);}}
                   >
                     {ci.name}
                     {
-                      city==cityId ?
+                      city>=0 && city==cityId ?
                       states[state].cities[city].towns.map((town, tid) => (
                         <div className="town" id={"town" + (tid+1)} key={town.name}>
                           {town.name}
@@ -195,11 +236,8 @@ function App() {
                 ))
               :null
             }
-          </div>
-        ))}
-
-      
-
+        </div>
+      ))}
     </div>
   );
 }
